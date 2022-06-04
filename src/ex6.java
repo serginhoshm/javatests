4604a926fbbfb13db4f66c132b1d4ce69ea8666b
Sérgio Marchiori Olist <sergio.marchiori@olist.com>
2022-06-04 11:22:12 -0300

    remover exception

--- src/ex6.java
+++ src/ex6.java
@@ -3,7 +3,7 @@
 import java.sql.*;
 
 public class ex6 {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         //int a = 16;
         //int b = 12;
         //int c = 7;
