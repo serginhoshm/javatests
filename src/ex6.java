1fea84607fd318ee9c1216ca743fa39ea6021aa3
Sérgio Marchiori <serginhoshm@gmail.com>
2022-06-04 10:38:57 -0300

    Adicionando arquivo para teste de conflitos

---
+++ src/ex6.java
@@ -0,0 +1,36 @@
+//import xerial.sqlite.jdbc;
+
+import java.sql.*;
+
+public class ex6 {
+    public static void main(String[] args) throws SQLException {
+        //int a = 16;
+        //int b = 12;
+        //int c = 7;
+        System.out.println("Arquivo para teste de conflitos");
+        //Sou um usuário que comitou este fonte pela primeira vez
+
+
+
+        //System.out.println((a+b) / c);
+        //String sqlite_file = "/home/sergio.marchiori/repos/pde/pde_2022.db";
+        //Connection connection = DriverManager.getConnection("jdbc:sqlite:" + sqlite_file);
+
+        //Statement statement = connection.createStatement();
+        //statement.setQueryTimeout(30);  // set timeout to 30 sec...
+
+        //ResultSet rs = statement.executeQuery("select * from temas order by id");
+        /*while(rs.next())
+        {
+            // read the result set
+            System.out.println("tema = " + rs.getString("tema"));
+            System.out.println("id = " + rs.getInt("id"));
+            // só um comentário de novo
+
+
+        }*/
+
+
+    }
+}
+
